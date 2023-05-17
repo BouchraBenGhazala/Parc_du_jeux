e33a4ea033e653bb8bfd15c9532c279b3f19926d
BouchraBenGhazala <ghazalabouchra@gmail.com>
2023-05-17 14:02:02 +0200

    test2

--- navbar.php
+++ navbar.php
@@ -21,16 +21,6 @@
                 <li class="nav-item">
                   <a class="nav-link " href="acceuil.php"><span >Le parc</span></a>
                 </li>
-
-                <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown link
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
         
                 <li class="nav-item">
                   <a class="nav-link" href="#">Infos pratiques</a>
