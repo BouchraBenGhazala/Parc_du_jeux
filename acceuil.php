2fc0a24474d7cc1f9a02af8651fcdfd1b7fbb46e
BouchraBenGhazala <ghazalabouchra@gmail.com>
2023-05-21 15:34:19 +0200

    TEST acceuil.php

--- acceuil.php
+++ acceuil.php
@@ -6,7 +6,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Main Page</title>
+        <title>MAINNN Page</title>
         <link rel="stylesheet" href="style.css">
         <link rel="stylesheet" href="Bootstrap files/css/bootstrap.min.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet">
